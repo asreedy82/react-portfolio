fcda8e7f5ce4b8bdd6a77dba26121f8bf0e333f3
areedy <a.reedy@gmail.com>
2023-03-01 23:57:39 -0500

    adding more to my navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from 'bootstrap';
 
 const Header = () => {
 
@@ -7,8 +8,27 @@ const Header = () => {
     }
 
     return (
-        <nav class="navbar navbar-light bg-light" style={navbarStyle}>
-            <span class="navbar-brand mb-0 h1">Anthony Reedy</span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={navbarStyle}>
+            <a className="navbar-brand">Anthony Reedy</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About Me </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Résumé</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'normalize.css';
+
+const script = document.createElement('script');
+script.src =
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js';
+script.src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js';
+script.src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js';
+script.async = true;
+document.body.appendChild(script);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
